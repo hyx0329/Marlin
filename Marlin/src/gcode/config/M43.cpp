hyx0329/Marlin

commit 613ff2087d93422cb4e48c4468f1878cc5a8ae44

✨ Code updates

Marlin/src/gcode/config/M43.cpp
@@ -63,19 +63,21 @@ inline void toggle_pins() {
 
   for (uint8_t i = start; i <= end; ++i) {
     pin_t pin = GET_PIN_MAP_PIN_M43(i);
-    if (!VALID_PIN(pin)) continue;
+    if (!isValidPin(pin)) continue;
     if (M43_NEVER_TOUCH(i) || (!ignore_protection && pin_is_protected(pin))) {
-      report_pin_state_extended(pin, ignore_protection, true, F("Untouched "));
+      printPinStateExt(pin, ignore_protection, true, F("Untouched "));
       SERIAL_EOL();
     }
     else {
       hal.watchdog_refresh();
-      report_pin_state_extended(pin, ignore_protection, true, F("Pulsing   "));
-      #ifdef __STM32F1__
-        const auto prior_mode = _GET_MODE(i);
-      #else
-        const bool prior_mode = GET_PINMODE(pin);
-      #endif
+      printPinStateExt(pin, ignore_protection, true, F("Pulsing   "));
+      const auto prior_mode = (
+        #ifdef __STM32F1__
+          _GET_MODE(i)
+        #else
+          getValidPinMode(pin)
+        #endif
+      );
       #if AVR_AT90USB1286_FAMILY // Teensy IDEs don't know about these pins so must use FASTIO
         if (pin == TEENSY_E2) {
           SET_OUTPUT(TEENSY_E2);
@@ -118,7 +120,7 @@ inline void toggle_pins() {
 
 inline void servo_probe_test() {
 
-  #if !(NUM_SERVOS > 0 && HAS_SERVO_0)
+  #if !HAS_SERVO_0
 
     SERIAL_ERROR_MSG("SERVO not set up.");
 
@@ -139,24 +141,15 @@ inline void servo_probe_test() {
     bool deploy_state = false, stow_state;
 
     #if ENABLED(Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN)
-
       #define PROBE_TEST_PIN Z_MIN_PIN
-      constexpr bool probe_inverting = Z_MIN_ENDSTOP_INVERTING;
-
-      SERIAL_ECHOLNPGM(". Probe Z_MIN_PIN: ", PROBE_TEST_PIN);
-      SERIAL_ECHOPGM(". Z_MIN_ENDSTOP_INVERTING: ");
-
+      #define _PROBE_PREF "Z_MIN"
     #else
-
       #define PROBE_TEST_PIN Z_MIN_PROBE_PIN
-      constexpr bool probe_inverting = Z_MIN_PROBE_ENDSTOP_INVERTING;
-
-      SERIAL_ECHOLNPGM(". Probe Z_MIN_PROBE_PIN: ", PROBE_TEST_PIN);
-      SERIAL_ECHOPGM(   ". Z_MIN_PROBE_ENDSTOP_INVERTING: ");
-
+      #define _PROBE_PREF "Z_MIN_PROBE"
     #endif
 
-    serialprint_truefalse(probe_inverting);
+    SERIAL_ECHOLNPGM(". Probe " _PROBE_PREF "_PIN: ", PROBE_TEST_PIN);
+    serial_ternary(F(". " _PROBE_PREF "_ENDSTOP_HIT_STATE: "), PROBE_HIT_STATE, F("HIGH"), F("LOW"));
     SERIAL_EOL();
 
     SET_INPUT_PULLUP(PROBE_TEST_PIN);
@@ -205,7 +198,7 @@ inline void servo_probe_test() {
         stow_state = READ(PROBE_TEST_PIN);
       }
 
-      if (probe_inverting != deploy_state) SERIAL_ECHOLNPGM("WARNING: INVERTING setting probably backwards.");
+      if (probe_inverting != deploy_state) SERIAL_ECHOLNPGM("WARNING: " _PROBE_PREF "_ENDSTOP_HIT_STATE is probably wrong.");
 
       if (deploy_state != stow_state) {
         SERIAL_ECHOLNPGM("= Mechanical Switch detected");
@@ -337,14 +330,14 @@ void GcodeSuite::M43() {
     bool can_watch = false;
     for (uint8_t i = first_pin; i <= last_pin; ++i) {
       pin_t pin = GET_PIN_MAP_PIN_M43(i);
-      if (!VALID_PIN(pin)) continue;
+      if (!isValidPin(pin)) continue;
       if (M43_NEVER_TOUCH(i) || (!ignore_protection && pin_is_protected(pin))) continue;
       can_watch = true;
       pinMode(pin, INPUT_PULLUP);
       delay(1);
       /*
-      if (IS_ANALOG(pin))
-        pin_state[pin - first_pin] = analogRead(DIGITAL_PIN_TO_ANALOG_PIN(pin)); // int16_t pin_state[...]
+      if (isAnalogPin(pin))
+        pin_state[pin - first_pin] = analogRead(digitalPinToAnalogIndex(pin)); // int16_t pin_state[...]
       else
       //*/
         pin_state[i - first_pin] = extDigitalRead(pin);
@@ -380,17 +373,17 @@ void GcodeSuite::M43() {
     for (;;) {
       for (uint8_t i = first_pin; i <= last_pin; ++i) {
         const pin_t pin = GET_PIN_MAP_PIN_M43(i);
-        if (!VALID_PIN(pin)) continue;
+        if (!isValidPin(pin)) continue;
         if (M43_NEVER_TOUCH(i) || (!ignore_protection && pin_is_protected(pin))) continue;
         const byte val =
           /*
-          IS_ANALOG(pin)
-            ? analogRead(DIGITAL_PIN_TO_ANALOG_PIN(pin)) : // int16_t val
+          isAnalogPin(pin)
+            ? analogRead(digitalPinToAnalogIndex(pin)) : // int16_t val
             :
           //*/
             extDigitalRead(pin);
         if (val != pin_state[i - first_pin]) {
-          report_pin_state_extended(pin, ignore_protection, true);
+          printPinStateExt(pin, ignore_protection, true);
           pin_state[i - first_pin] = val;
         }
       }
@@ -409,7 +402,7 @@ void GcodeSuite::M43() {
     // Report current state of selected pin(s)
     for (uint8_t i = first_pin; i <= last_pin; ++i) {
       const pin_t pin = GET_PIN_MAP_PIN_M43(i);
-      if (VALID_PIN(pin)) report_pin_state_extended(pin, ignore_protection, true);
+      if (isValidPin(pin)) printPinStateExt(pin, ignore_protection, true);
     }
   }
 }

commit d7499626f1c26efc88a2b3c05ad86c692a13550b

DO_NOT_MERGE: fix M43 code for 2.1.2.5
- possibly broken by 613ff2087d93422cb4e48c4468f1878cc5a8ae44(code
  update)
- partially revert changes to M43.cpp

Marlin/src/gcode/config/M43.cpp
@@ -141,15 +141,26 @@ inline void servo_probe_test() {
     bool deploy_state = false, stow_state;
 
     #if ENABLED(Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN)
+
       #define PROBE_TEST_PIN Z_MIN_PIN
+      constexpr bool probe_inverting = Z_MIN_ENDSTOP_INVERTING;
+
+      SERIAL_ECHOLNPGM(". Probe Z_MIN_PIN: ", PROBE_TEST_PIN);
+      SERIAL_ECHOPGM(". Z_MIN_ENDSTOP_INVERTING: ");
+
       #define _PROBE_PREF "Z_MIN"
     #else
+
       #define PROBE_TEST_PIN Z_MIN_PROBE_PIN
+      constexpr bool probe_inverting = Z_MIN_PROBE_ENDSTOP_INVERTING;
+
+      SERIAL_ECHOLNPGM(". Probe Z_MIN_PROBE_PIN: ", PROBE_TEST_PIN);
+      SERIAL_ECHOPGM(   ". Z_MIN_PROBE_ENDSTOP_INVERTING: ");
+
       #define _PROBE_PREF "Z_MIN_PROBE"
     #endif
 
-    SERIAL_ECHOLNPGM(". Probe " _PROBE_PREF "_PIN: ", PROBE_TEST_PIN);
-    serial_ternary(F(". " _PROBE_PREF "_ENDSTOP_HIT_STATE: "), PROBE_HIT_STATE, F("HIGH"), F("LOW"));
+    serialprint_truefalse(probe_inverting);
     SERIAL_EOL();
 
     SET_INPUT_PULLUP(PROBE_TEST_PIN);